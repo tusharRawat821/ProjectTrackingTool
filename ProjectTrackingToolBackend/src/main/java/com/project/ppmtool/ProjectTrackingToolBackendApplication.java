aea4a2e76615e6c1402b0f14f283f1b8281913ba
Tushar Rawat <tusharrawat821@gmail.com>
2021-11-21 15:15:47 +0530

    first commit

---
+++ ProjectTrackingToolBackend/src/main/java/com/project/ppmtool/ProjectTrackingToolBackendApplication.java
@@ -0,0 +1,13 @@
+package com.project.ppmtool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectTrackingToolBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectTrackingToolBackendApplication.class, args);
+	}
+
+}
